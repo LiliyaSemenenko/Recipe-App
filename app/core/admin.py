98e05c60ed469a2401425bdb454c684963ec096c
Liliya Semenenko <lilinikcopy@gmail.com>
2023-07-17 11:43:53 -0700

    Setup costumized Django Admin and tests.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,56 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+# UserAdmin: base class used for default django authentication system
+# BaseUserAdmin: named like that because we don't want our UserAdmin and this one to conflict bcs of same names
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# integrates with django translation system (automatically translating the text)
+from django.utils.translation import gettext_lazy as _ # _(section_to_translate
 
-# Register your models here.
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+
+    ordering = ['id'] # order users by id
+    list_display = ['email', 'name'] # list these items
+
+    # ((title, {'fields': ('','')}),)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}), # title = None
+        (_('Personal Info'), {'fields': ('name',)}), # title = Personal Info
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+
+    readonly_fields = ['last_login']
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',), # used for page look
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+# register User model and assign custom UserAdmin class
+admin.site.register(models.User, UserAdmin)
