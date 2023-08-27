8c681f05e9736503b038458b50e9b986a51b2749
Liliya Semenenko <lilinikcopy@gmail.com>
2023-08-27 15:57:50 -0700

    Implemented rating component in frontend.

--- frontend/src/components/Recipe.js
+++ frontend/src/components/Recipe.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-// import Likes from './Likes'
+import Rating from './Rating'
 import { Link } from 'react-router-dom'
 
 function Recipe({ recipe }) {
@@ -12,20 +12,20 @@ function Recipe({ recipe }) {
 
             <Card.Body>
                 <Link to={`/recipe/${recipe._id}`}>
-                    <Card.Title as="div">
+                    <Card.Title as="h3">
                         <strong>{recipe.name}</strong>
                     </Card.Title>
                 </Link>
 
                 <Card.Text as="div">
                     <div className="my-3">
-                        {recipe.rating} likes {recipe.numReviews} comments
-                        {/* <Likes value={recipe.likes} text={`${recipe.numReviews} reviews`} color={'#f8e825'} /> */}
+                        {/* {recipe.rating} likes {recipe.numReviews} comments */}
+                        <Rating value={recipe.rating} text={`${recipe.numComments} reviews`} color={'#f8e825'} />
                     </div>
                 </Card.Text>
 
 
-                <Card.Text as="h3">
+                <Card.Text as="div">
                     @{recipe.username}
                 </Card.Text>
 
